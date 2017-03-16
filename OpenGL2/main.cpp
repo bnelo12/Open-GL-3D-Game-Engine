81ba4b3e3c1ff7bddcad0739144459cffe32b13f
Benjamin Elo <Ben@pc-132-236.customer.brae.knightsbridge.lan>
2017-03-16 18:32:24 +0000

    Initial Commit

---
+++ OpenGL2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL2
+//
+//  Created by Benjamin Elo on 3/16/17.
+//  Copyright © 2017 Elo Software. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
